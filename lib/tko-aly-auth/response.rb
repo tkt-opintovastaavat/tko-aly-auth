c6ec632495e7c65307de4f615bc4cc25ce8afa5a
Sami Saada <saada@cs.helsinki.fi>
2011-09-15 01:44:43 +0300

    added response parsing

--- lib/tko-aly-auth/response.rb
+++ lib/tko-aly-auth/response.rb
@@ -1,8 +1,27 @@
+require 'xml'
+
 module TKOaly
   module Auth
     class Response
 
-      def initialize(request)
+      attr_reader :code, :username
+
+      def initialize(response)
+        @code = response.code.to_i
+        @username = parse_username(response)
+      end
+
+      def belongs?(username)
+        @code == 200 and @username == username
+      end
+
+      private
+
+      def parse_username(response)
+        xml = XML::Document.string(response.body)
+        user = xml.find('user').first
+        username = user.find('username').first
+        username.first.to_s #string
       end
 
     end
--- spec/auth_response_spec.rb
+++ spec/auth_response_spec.rb
@@ -2,12 +2,36 @@ require 'spec_helper'
 
 describe TKOaly::Auth::Response do
 
+  before(:each) do
+    @code = 200
+    @username = 'foobar'
+    @xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<auth_response>\n\t<status>\n\t\t<code>0</code>\n\t\t<message>Komento suoritettu</message>\n\t</status>\n\t<user>\n\t\t<timestamp>1316037301</timestamp>\n\t\t<username>#{@username}</username>\n\t\t<isRandomRole>true</isRandomRole>\n\t</user>\n\t</auth_response>\n"
+    @response = mock(Net::HTTPResponse)
+    @response.stub!(:code).and_return(@code)
+    @response.stub!(:body).and_return(@xml)
+    @auth_response = TKOaly::Auth::Response.new @response
+  end
+
   describe "creation" do
 
-    it "should be created with HTTP-response" do
-      request = Net::HTTPResponse.new 'foo', 'bar', 'baz'
-      response = TKOaly::Auth::Response.new request
-      response.should_not be_nil
+    it "should be created with HTTP-auth_response" do
+      @auth_response.should_not be_nil
+    end
+
+  end
+
+  describe "accessors" do
+
+    it "should have code" do
+      @auth_response.code.should == @code
+    end
+
+    it "should have username" do
+      @auth_response.username.should == @username
+    end
+
+    it "should tell if username belongs to role" do
+      @auth_response.belongs?(@username).should be_true
     end
 
   end
