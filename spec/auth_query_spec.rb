953fc501e2ab8bf7412b26defb7f581babad1b82
Sami Saada <saada@cs.helsinki.fi>
2011-09-15 00:21:18 +0300

    creation refactored

--- spec/auth_query_spec.rb
+++ spec/auth_query_spec.rb
@@ -2,15 +2,20 @@ require 'spec_helper'
 
 describe TKOaly::Auth::Query do
 
+  before(:each) do
+    @username = 'johndoe'
+    @role = 'RandomRole'
+    @query = TKOaly::Auth::Query.new @username, @role
+  end
+
   describe "creation" do
 
-    it "should take username and role" do
-      username = 'johndoe'
-      role = 'randomrole'
+    it "should have username" do
+      @query.username.should == @username
+    end
 
-      query = TKOaly::Auth::Query.new username, role
-      query.username.should == username
-      query.role.should == role
+    it "should have role" do
+      @query.role.should == @role
     end
 
   end
