5bfc2126f84078fd775c1743a8045b67bb989ed7
Sami Saada <saada@cs.helsinki.fi>
2011-09-14 23:55:24 +0300

    added rspec

---
+++ spec/basic_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'basic test' do
+
+  it 'should tell if rspec works' do
+    'rspec'.should_not == 'unittest'
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'tko-aly-auth'
+
+RSpec.configure do |config|
+  # some (optional) config here
+end
