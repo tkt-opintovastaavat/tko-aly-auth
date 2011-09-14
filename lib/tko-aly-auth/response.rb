9bb4b7759ed0f0474d85b3ad4b3a5988cba9ae19
Sami Saada <saada@cs.helsinki.fi>
2011-09-15 01:06:01 +0300

    added initializer

--- lib/tko-aly-auth/response.rb
+++ lib/tko-aly-auth/response.rb
@@ -1,6 +1,10 @@
 module TKOaly
   module Auth
     class Response
+
+      def initialize(request)
+      end
+
     end
   end
 end
--- spec/auth_response_spec.rb
+++ spec/auth_response_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe TKOaly::Auth::Response do
+
+  describe "creation" do
+
+    it "should be created with HTTP-response" do
+      request = Net::HTTPResponse.new 'foo', 'bar', 'baz'
+      response = TKOaly::Auth::Response.new request
+      response.should_not be_nil
+    end
+
+  end
+
 end
