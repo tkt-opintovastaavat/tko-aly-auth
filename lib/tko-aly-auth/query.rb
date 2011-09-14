5e28d19ad106a8525bf52671e7574e447b9ca3b1
Sami Saada <saada@cs.helsinki.fi>
2011-09-15 00:07:19 +0300

    added query class with constructor

--- lib/tko-aly-auth/query.rb
+++ lib/tko-aly-auth/query.rb
@@ -1,6 +1,14 @@
 module TKOaly
   module Auth
     class Query
+
+      attr_accessor :username, :role
+
+      def initialize(username, role)
+        @username = username
+        @role = role
+      end
+
     end
   end
 end
--- spec/auth_query_spec.rb
+++ spec/auth_query_spec.rb
@@ -1,4 +1,18 @@
 require 'spec_helper'
 
 describe TKOaly::Auth::Query do
+
+  describe "creation" do
+
+    it "should take username and role" do
+      username = 'johndoe'
+      role = 'randomrole'
+
+      query = TKOaly::Auth::Query.new username, role
+      query.username.should == username
+      query.role.should == role
+    end
+
+  end
+
 end
